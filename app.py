aff18f338dfd14ca8cae6d967b66f3bb2d3069e5
Iranzi Dev <vpnzoe27@gmail.com>
2023-07-19 19:13:13 +0200

    First Commit

--- app.py
+++ app.py
@@ -10,18 +10,19 @@ app = Flask(__name__)
 
 
 
-def search_song(user_input):
+    
+
+def download_song(user_input):
     search_query = user_input
     results = YoutubeSearch(search_query, max_results=1).to_dict()
 
     for video in results:
         video_title = video["title"]
         video_url = "https://www.youtube.com/watch?v=" + video["id"]
-        print(f"{video_title}: {video_url}")
 
     file_path = os.path.join("static", "songs", f"{video_title}.mp3")
     if os.path.exists(file_path):
-        response = f"song++{file_path}"
+        response = file_path
         return response
     else:
         ydl_opts = {
@@ -33,9 +34,8 @@ def search_song(user_input):
             ydl.download([video_url])
 
         print(f"Download complete: {video_title}")
-        response = f"song++{file_path}"
+        response = file_path
         return response
-
 
 # Define the route for the home page
 @app.route("/")
@@ -49,13 +49,31 @@ def serve_song(filename):
     return send_from_directory("static/songs", filename)
 
 
-@app.route("/response", methods=["POST"])
+@app.route("/request", methods=["POST"])
 def process():
-    user_input = request.form["user_input"].lower()
+    song_request = request.form["user_input"].lower()
+    results = YoutubeSearch(song_request, max_results=5).to_dict()
 
-    response = search_song(user_input)
+    songs = []
+    for video in results:
+        video_title = video["title"]
+        songs.append(f"{video_title}")
 
-    return jsonify({"response": response})
+    return jsonify({"songs": songs})
+
+
+    
+
+
+
+@app.route("/song_request", methods=["POST"])
+def return_song():
+    song_request = request.form["song_request"].lower()
+    song = download_song(song_request)
+
+
+    return jsonify({"song": song})
+
 
 
 if __name__ == "__main__":
